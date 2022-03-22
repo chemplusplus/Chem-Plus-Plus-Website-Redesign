0e10d36ec305a54b916200b96e9c48c6436b3c5d
Julian <julianfrig@outlook.com>
2022-03-21 17:36:24 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 window.addEventListener("load", function() {
 	console.log("Page Loaded Successfully"); //Logs a message to console when the page loads
 });
+
+less.logger.addListener({
+    debug: function(msg) {
+    },
+    info: function(msg) {
+    },
+    warn: function(msg) {
+    },
+    error: function(msg) {
+    }
+});
